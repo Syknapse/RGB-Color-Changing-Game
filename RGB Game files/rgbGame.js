af1c87b9390d3e303a89e26c600f64cb6b5341c7
Syk Houdeib <syk.houdeib@gmail.com>
2017-07-18 19:55:33 +0200

    Second session
    Added random color functions: get random color, push to array, randomly select from array.
    Added h1 background change.

--- RGB Game files/rgbGame.js
+++ RGB Game files/rgbGame.js
@@ -1,21 +1,15 @@
 // six square colors
-var colors = [
-	"rgb(255, 0, 0)", 
-	"rgb(255, 255, 0)",
-	"rgb(0, 255, 0)",
-	"rgb(0, 255, 255)",
-	"rgb(0, 0, 255)",
-	"rgb(255, 0, 255)",
-]
-
+var colors = generateRandomColors(6);
 // select the squares 
 var squares = document.querySelectorAll(".square");
 // the target color in h1
-var pickedColor = colors[3];
-// select ^ the h1 target color
+var pickedColor = pickColor();
+// select the h1 target color
 var colorDisplay = document.getElementById("colorDisplay");
 // select the message (try again/correct display)
 var messageDisplay = document.querySelector("#message");
+//select h1 (to set background color)
+var h1 = document.querySelector("h1");
 
 // update color display to say the picked color
 colorDisplay.textContent = pickedColor;
@@ -33,6 +27,7 @@ for(var i = 0; i < squares.length; i++){
 		if(clickedColor === pickedColor){
 			messageDisplay.textContent = "Correct! Well Done";
 			changeColors(clickedColor);
+			h1.style.backgroundColor = clickedColor;
 		} else {
 			this.style.backgroundColor = "#232323";
 			messageDisplay.textContent = "Try Again";
@@ -48,4 +43,35 @@ function changeColors(color){
 	squares[i].style.backgroundColor = color;
 	}
 	
+}
+
+// function to randomly select a color from the colors array
+function pickColor(){
+	var random = Math.floor(Math.random() * colors.length);
+	return colors[random];
+}
+
+// function to generate random colors in the colors array
+function generateRandomColors(num){
+	//make an array
+	var arr = []
+	// add num random colors to array
+	// repeat num times
+	for(var i = 0; i < num; i++){
+		//get random color and push into arr
+		arr.push(randomColor())
+	}
+	//return that array
+	return arr;
+}
+
+// function to get random color
+function randomColor(){
+	//pick a "red" from 0 - 255
+	var r = Math.floor(Math.random() * 256);
+	//pick a "green" from 0 - 255
+	var g = Math.floor(Math.random() * 256);
+	//pick a "blue" from 0 - 255
+	var b = Math.floor(Math.random() * 256);
+	return "rgb(" + r + ", " + g + ", " + b + ")";
 }
