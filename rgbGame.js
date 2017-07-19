b85f3b88f3b8cd9a15407acac293246ec84e40f3
Syk Houdeib <syk.houdeib@gmail.com>
2017-07-19 20:15:18 +0200

    CSS main styling
    fonts, squares border radius, margins, h1, stripe, buttons, hover, transitions.

--- rgbGame.js
+++ rgbGame.js
@@ -62,7 +62,7 @@ resetButton.addEventListener("click", function(){
 	for (var i = 0; i < squares.length; i++) {
 		squares[i].style.backgroundColor = colors[i];
 	}
-	h1.style.backgroundColor = "#232323";
+	h1.style.backgroundColor = "steelblue";
 });
 
 // update color display to say the picked color
